3c68d8bc414a7e64fa878e81ada3305dca374b32
jordanhudgens <jordan@devcamp.com>
2018-07-30 15:45:53 -0700

    Added sample component along with nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { DocumentComponent } from './document/document.component';
+import { SectionComponent } from './document/section/section.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DocumentComponent, SectionComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
